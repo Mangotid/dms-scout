74ef2e758e93c34d49a13530289a0dbf51c0fe18
Twis112 <106381738+Mangotid@users.noreply.github.com>
2026-06-10 14:50:18 +0300

    Update versionCode to 2 in build.gradle.kts1,1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "ua.universalna.dmsscout"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1.1
+        versionCode = 2
         versionName = "1.1"
     }
     buildTypes {
